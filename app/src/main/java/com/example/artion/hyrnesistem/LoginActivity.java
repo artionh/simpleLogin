97d14702bf5ec45f95b5903406b77b1ea945a3ad
artionh <artion811@gmail.com>
2017-04-03 20:55:55 +0200

    Update LoginActivity.java

--- app/src/main/java/com/example/artion/hyrnesistem/LoginActivity.java
+++ app/src/main/java/com/example/artion/hyrnesistem/LoginActivity.java
@@ -1,8 +1,8 @@
 package com.example.artion.hyrnesistem;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,22 +21,56 @@ public class LoginActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        init();
         login();
+        register();
+    }
+
+    public void init(){
+        email = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
+        login = (Button) findViewById(R.id.hyr);
+        register = (Button) findViewById(R.id.regjistrohu);
+    }
+
+    public void checkLogin(){
+        inEmail = email.getText().toString();
+        inPassword = password.getText().toString();
+        String message = "";
+        if(inEmail.isEmpty() && inPassword.isEmpty()){
+            message = "Fushat nuk mund te jene bosh!";
+            Toast.makeText(getApplicationContext(),message,Toast.LENGTH_SHORT).show();
+        }
+        else if(!(inEmail.isEmpty()) && inPassword.isEmpty()){
+            message = "Fjalekalimi nuk mund te jete bosh!";
+            Toast.makeText(getApplicationContext(),message,Toast.LENGTH_SHORT).show();
+        }
+        else if(inEmail.isEmpty() && !(inPassword.isEmpty())){
+            message = "E-mail nuk mund te jete bosh!";
+            Toast.makeText(getApplicationContext(),message,Toast.LENGTH_SHORT).show();
+        }
+        else{
+            message = "Ju futet: "+inEmail+" dhe: "+inPassword;
+            Toast.makeText(getApplicationContext(),message,Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(getApplicationContext(),ProfileActivity.class);
+            Bundle b = new Bundle();
+            b.putString("email",inEmail);
+            b.putString("password",inPassword);
+            i.putExtras(b);
+            startActivity(i);
+        }
     }
 
     public void login(){
-        email = (EditText) findViewById(R.id.email);
-        password = (EditText) findViewById(R.id.password);
-        inEmail = email.toString();
-        inPassword = password.toString();
-        login = (Button) findViewById(R.id.hyr);
-        register = (Button) findViewById(R.id.regjistrohu);
         login.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                String success = "Login u krye me sukses";
-                Toast.makeText(LoginActivity.this, success, Toast.LENGTH_SHORT).show();
+                checkLogin();
             }
         });
+
+    }
+
+    public void register(){
         register.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
